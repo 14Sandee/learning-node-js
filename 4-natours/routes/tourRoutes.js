8cd253f61d2a1b53ad8bca0cf141b1304a7cd012
14Sandee <sandee.vit14@gmail.com>
2024-02-07 13:08:48 +0530

    feat: added sorting, filtering, limit, pagination in API

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getAllTours, getTour, createTour, updateTour, deleteTour, checkID, checkBody } = require('../controllers/tourController');
+const { getAllTours, getTour, createTour, updateTour, deleteTour, checkID, checkBody, aliasTopTour } = require('../controllers/tourController');
 
 const router = express.Router();
 
 // router.param('id', checkID);
+
+router.route('/top-5-cheap').get(aliasTopTour, getAllTours);
 
 router.route('/').get(getAllTours).post(createTour);
 
