64b0df72471e8cfead05692479a8ea3d5483f85b
14Sandee <sandee.vit14@gmail.com>
2024-02-08 12:58:18 +0530

    feat: Aggregation Pipeline

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const { getAllTours, getTour, createTour, updateTour, deleteTour, checkID, checkBody, aliasTopTour } = require('../controllers/tourController');
+const { getAllTours, getTour, createTour, updateTour, deleteTour, checkID, checkBody, aliasTopTour, getTourStats, getMonthlyPlan } = require('../controllers/tourController');
 
 const router = express.Router();
 
 // router.param('id', checkID);
 
 router.route('/top-5-cheap').get(aliasTopTour, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/').get(getAllTours).post(createTour);
 
