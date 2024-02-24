2757f84f8fa16fcace5d25d5ee1313baaa804ad1
14Sandee <sandee.vit14@gmail.com>
2024-02-24 22:41:14 +0530

    remove: comments

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -9,17 +9,6 @@ mongoose.connect(DB).then(() => {
     console.log('DB connection successful!');
 });
 
-// const testTour = new Tour({
-//     name: 'The Park Camper',
-//     price: 997
-// })
-
-// testTour.save().then(doc => {
-//     console.log(doc);
-// }).catch(err => {
-//     console.log('ERROR: ', err);
-// })
-
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
