c11c0355a000e18c9e81d4bc8cbb5778c11bf6b7
14Sandee <sandee.vit14@gmail.com>
2024-02-26 09:42:51 +0530

    added: handling error for Invalid DB IDs

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError')
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}.`
+    return new AppError(message, 400)
+}
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -32,9 +39,10 @@ module.exports = ((err, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res)
     } else if (process.env.NODE_ENV === 'production') {
-        sendErrorProd(err, res)
+        let error = { ...err }
+        if (error.name === 'CastError') error = handleCastErrorDB(error)
+
+        sendErrorProd(error, res)
     }
 
 })
-
-// TODO: 5. Implementing a Global Error Handling Middleware
