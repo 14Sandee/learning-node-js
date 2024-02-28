6d964850d4a8e5315adab0d13376970d1af14565
14Sandee <sandee.vit14@gmail.com>
2024-02-28 16:58:24 +0530

    added: delete user data API

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllUsers, getUser, createUser, deleteUser, updateUser, updateMe } = require('../controllers/userController')
+const { getAllUsers, getUser, createUser, deleteUser, updateUser, updateMe, deleteMe } = require('../controllers/userController')
 const { signup, login, forgotPassword, resetPassword, updatePassword, protect } = require('../controllers/authController');
 
 const router = express.Router();
@@ -13,6 +13,7 @@ router.patch('/resetPassword/:token', resetPassword);
 router.patch('/updateMyPassword', protect, updatePassword);
 
 router.patch('/updateMe', protect, updateMe);
+router.delete('/deleteMe', protect, deleteMe);
 
 router.route('/').get(getAllUsers).post(createUser);
 
