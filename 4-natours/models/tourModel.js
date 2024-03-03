34c6fae19c37e7dc4167e823a5c50159f0f4a1a0
14Sandee <sandee.vit14@gmail.com>
2024-03-03 16:13:10 +0530

    Populating Tour Guides

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -120,6 +120,26 @@ tourSchema.pre('save', function (next) {
 //     next();
 // })
 
+// QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function (next) {
+    this.find({ secretTour: { $ne: true } });
+    this.start = Date.now();
+    next();
+})
+
+tourSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'guides',
+        select: '-__v -passwordChangedAt'
+    });
+    next();
+})
+
+tourSchema.post(/^find/, function (docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds`);
+    next();
+})
+
 // tourSchema.post('save', function (doc, next) {
 //     console.log(doc);
 //     next();
