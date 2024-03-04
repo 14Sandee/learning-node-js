4471bc8782584105354b39166d011147c710e18f
14Sandee <sandee.vit14@gmail.com>
2024-03-04 11:22:55 +0530

    added: missing authentication and authorization

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -9,14 +9,23 @@ const router = express.Router();
 
 router.use('/:tourId/reviews', reviewRouter);
 
-router.route('/top-5-cheap').get(aliasTopTour, getAllTours);
+router.route('/top-5-cheap')
+    .get(aliasTopTour, getAllTours);
 
-router.route('/tour-stats').get(getTourStats);
-router.route('/monthly-plan/:year').get(getMonthlyPlan);
+router.route('/tour-stats')
+    .get(getTourStats);
 
-router.route('/').get(protect, getAllTours).post(createTour);
+router.route('/monthly-plan/:year')
+    .get(protect, restrictTo('admin', 'lead-guide', 'guide'), getMonthlyPlan);
 
-router.route('/:id').get(getTour).patch(updateTour).delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
+router.route('/')
+    .get(getAllTours)
+    .post(protect, restrictTo('admin', 'lead-guide'), createTour);
+
+router.route('/:id')
+    .get(getTour)
+    .patch(protect, restrictTo('admin', 'lead-guide'), updateTour)
+    .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
 
 
 module.exports = router;
--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -10,14 +10,16 @@ router.post('/login', login);
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
 
-router.patch('/updateMyPassword', protect, updatePassword);
+// Protect all routes after this middleware
+router.use(protect);
+router.patch('/updateMyPassword', updatePassword);
+router.get('/me', getMe, getUser);
+router.patch('/updateMe', updateMe);
+router.delete('/deleteMe', deleteMe);
 
-router.get('/me', protect, getMe, getUser);
-router.patch('/updateMe', protect, updateMe);
-router.delete('/deleteMe', protect, deleteMe);
-
+// Restrict all routes after this middleware to admin only
+router.use(restrictTo('admin'));
 router.route('/').get(getAllUsers).post(createUser);
-
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
