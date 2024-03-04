12897c5897d58d560a56d215228b64d3d2f25825
14Sandee <sandee.vit14@gmail.com>
2024-03-04 08:04:14 +0530

    handle factory function for delete

--- 4-natours/routes/reviewRoutes.js
+++ 4-natours/routes/reviewRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createReview, getAllReviews } = require('../controllers/reviewController');
+const { createReview, getAllReviews, deleteReview } = require('../controllers/reviewController');
 const { protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/').post(protect, restrictTo('user'), createReview).get(getAllReviews);
+
+router.route('/:id').delete(deleteReview);
 
 module.exports = router; 
