170c47c1e53a3ee0d1f6fbc98f6a5ebbf0c99f2f
14Sandee <sandee.vit14@gmail.com>
2024-03-04 08:14:56 +0530

    added: handler function to update

--- 4-natours/routes/reviewRoutes.js
+++ 4-natours/routes/reviewRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { createReview, getAllReviews, deleteReview } = require('../controllers/reviewController');
+const { createReview, getAllReviews, deleteReview, updateReview } = require('../controllers/reviewController');
 const { protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/').post(protect, restrictTo('user'), createReview).get(getAllReviews);
 
-router.route('/:id').delete(deleteReview);
+router.route('/:id').patch(updateReview).delete(deleteReview);
 
 module.exports = router; 
