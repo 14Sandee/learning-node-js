7dfbead767bb4c2d4000f52279f2a1cb33663e67
14Sandee <sandee.vit14@gmail.com>
2024-03-05 17:57:02 +0530

    added: login render changes

--- 4-natours/public/js/login.js
+++ 4-natours/public/js/login.js
@@ -9,9 +9,15 @@ const login = async (email, password) => {
                 password
             }
         });
-        console.log(res)
+
+        if (res.data.status === 'success') {
+            alert('Logged in successfully!');
+            window.setTimeout(() => {
+                location.assign('/');
+            }, 1500);
+        }
     } catch (err) {
-        console.log(err.response.data.message);
+        alert(err.response.data.message);
     }
 }
 
