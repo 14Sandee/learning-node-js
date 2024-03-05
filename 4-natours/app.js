c4f471398a7fe5ab584c0fb81f24939f91ea607d
14Sandee <sandee.vit14@gmail.com>
2024-03-05 10:52:04 +0530

    extending base template with blocks

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -69,10 +69,22 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).render('base', {
         tour: 'The Forest Hiker',
         user: 'Jonas'
+    });
+});
+
+app.get('/overview', (req, res) => {
+    res.status(200).render('overview', {
+        title: 'All Tours'
+    });
+});
+
+app.get('/tour', (req, res) => {
+    res.status(200).render('tour', {
+        title: 'The Forest Hiker'
     });
 });
 
